739eda6529233f01efa2ca52fbe10042d2af4bd5
Guillex <lgsilva@uci.cu>
2021-04-07 17:36:12 -0400

    Adding Close action to foreground service notification.

--- app/src/main/java/cu/guillex/postrack/services/ForegroundService.kt
+++ app/src/main/java/cu/guillex/postrack/services/ForegroundService.kt
@@ -21,18 +21,34 @@ class ForegroundService : Service() {
     }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+
+        Log.i("MQTT", "ForegroundService - onStartCommand")
+
+        if (intent != null && intent.getBooleanExtra("Close_Action_Intent", false)) {
+            Log.i("MQTT", "Cerrar intent")
+            stopSelf()
+            return START_NOT_STICKY
+        }
+
         val pendingIntent: PendingIntent =
                 Intent(this, MainActivity::class.java).let { notificationIntent ->
                     PendingIntent.getActivity(this, 0, notificationIntent, 0)
                 }
 
-        var builder = NotificationCompat.Builder(this, "MqttChannel")
+        val serviceActionIntent : PendingIntent =
+                Intent(this, ForegroundService::class.java).let {
+                    it.putExtra("Close_Action_Intent", true)
+                    PendingIntent.getService(this, 0, it, 0)
+                }
+
+        val builder = NotificationCompat.Builder(this, "MqttChannel")
                 .setSmallIcon(R.drawable.ic_launcher_foreground)
                 .setContentTitle("Connection success")
                 .setContentText("Conectado al servidor MQTT.")
                 .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-                .setContentIntent(pendingIntent)
-                .setAutoCancel(true)
+                .setContentIntent(pendingIntent)    //Este Intent es el que levanta el activity cuando se toca la notificación
+                .addAction(R.drawable.ic_launcher_foreground, "Close", serviceActionIntent)
+                //.setAutoCancel(true)              //automatically removes the notification when the user taps it
 
         startForeground(1, builder.build())
 
